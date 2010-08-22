242d6c9ffc5f1aa75278536d39329489518f560f
NgTzeYang <ngty77@gmail.com>
2010-08-22 13:39:12 +0800

    Added specs for 'while' block & modifier.

---
+++ spec/proc/to_source_from_do_end_block_w_nested_while_spec.rb
@@ -0,0 +1,176 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+
+describe "Proc#to_source from do ... end block (w nested while block)" do
+
+  expected1 = %|proc {\n while(true) \n a = "ia" end\n  [xx, x, @x, @@x, $x] }|
+  expected2 = %|proc {\n a = 'ia' while true\n  [xx, x, @x, @@x, $x] }|
+
+  should 'handle watever(..) do ... end (w do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) do
+        while(true) do a = 'ia' end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever(..) do ... end (wo do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) do
+        while true
+          a = 'ia'
+        end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever(..) \ do ... end (w do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) \
+        do
+          while(true) do a = 'ia' end
+          [xx, x, @x, @@x, $x]
+        end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever(..) \ do ... end (wo do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) \
+        do
+          while true
+            a = 'ia'
+          end
+          [xx, x, @x, @@x, $x]
+        end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever do ... end (w do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever do
+        while(true) do a = 'ia' end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever do ... end (wo do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever do
+        while true
+          a = 'ia'
+        end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever \ do ... end (w do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever \
+        do
+          while(true) do a = 'ia' end
+          [xx, x, @x, @@x, $x]
+        end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever \ do ... end (wo do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever \
+        do
+          while true
+            a = 'ia'
+          end
+          [xx, x, @x, @@x, $x]
+        end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle lambda do ... end (w do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda do
+        while(true) do a = 'ia' end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle lambda do ... end (wo do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda do
+        while true
+          a = 'ia'
+        end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle lambda \ do ... end (w do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda \
+        do
+          while(true) do a = 'ia' end
+          [xx, x, @x, @@x, $x]
+        end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle lambda \ do ... end (wo do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda \
+        do
+          while true
+            a = 'ia'
+          end
+          [xx, x, @x, @@x, $x]
+        end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever(..) do ... end (as modifier)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) do
+        a = 'ia' while true
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected2)
+  end
+
+  should 'handle watever do ... end (as modifier)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever do
+        a = 'ia' while true
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected2)
+  end
+
+  should 'handle lambda do ... end (as modifier)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda do
+        a = 'ia' while true
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected2)
+  end
+
+end
