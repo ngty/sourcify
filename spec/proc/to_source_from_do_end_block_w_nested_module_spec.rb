04680c00eb7848470b903c6bc1ae34efaf4854e2
NgTzeYang <ngty77@gmail.com>
2010-08-22 14:30:59 +0800

    Added specs for nested module.

---
+++ spec/proc/to_source_from_do_end_block_w_nested_module_spec.rb
@@ -0,0 +1,42 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+
+describe "Proc#to_source from do ... end block (w nested module)" do
+
+  expected = %Q\
+    proc do
+      module AA; a = "ia"; end
+      [xx, x, @x, @@x, $x]
+    end
+  \
+
+  should 'handle watever(..) do ... end' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) do
+        module AA; a = 'ia'; end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected)
+  end
+
+  should 'handle watever do ... end' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever do
+        module AA; a = 'ia'; end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected)
+  end
+
+  should 'handle lambda do ... end' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda do
+        module AA; a = 'ia'; end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected)
+  end
+
+end
