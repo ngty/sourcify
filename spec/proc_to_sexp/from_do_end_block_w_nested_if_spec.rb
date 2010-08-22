9d66aff6c3d509e4e8889e02d70d28f93dcd1bdc
NgTzeYang <ngty77@gmail.com>
2010-08-23 00:17:12 +0800

    if & unless can work as modifier as well.

--- spec/proc_to_sexp/from_do_end_block_w_nested_if_spec.rb
+++ spec/proc_to_sexp/from_do_end_block_w_nested_if_spec.rb
@@ -2,12 +2,25 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe "Proc#to_sexp from do ... end block (w nested if)" do
 
-  expected =
+  expected1 =
     s(:iter,
       s(:call, nil, :proc, s(:arglist)),
       nil,
       s(:block,
         s(:if, s(:call, nil, :aa, s(:arglist)), s(:true), nil),
+             s(:array,
+               s(:call, nil, :xx, s(:arglist)),
+               s(:lvar, :x),
+               s(:ivar, :@x),
+               s(:cvar, :@@x),
+               s(:gvar, :$x))))
+
+  expected2 =
+    s(:iter,
+      s(:call, nil, :proc, s(:arglist)),
+      nil,
+      s(:block,
+        s(:if, s(:true), s(:lasgn, :a, s(:str, "ia")), nil),
              s(:array,
                s(:call, nil, :xx, s(:arglist)),
                s(:lvar, :x),
@@ -22,7 +35,7 @@ describe "Proc#to_sexp from do ... end block (w nested if)" do
         if aa then true ; end
         [xx, x, @x, @@x, $x]
       end
-    ).should.be having_sexp(expected)
+    ).should.be having_sexp(expected1)
   end
 
   should 'handle watever do ... end' do
@@ -32,7 +45,7 @@ describe "Proc#to_sexp from do ... end block (w nested if)" do
         if aa then true ; end
         [xx, x, @x, @@x, $x]
       end
-    ).should.be having_sexp(expected)
+    ).should.be having_sexp(expected1)
   end
 
   should 'handle lambda do ... end' do
@@ -42,7 +55,37 @@ describe "Proc#to_sexp from do ... end block (w nested if)" do
         if aa then true ; end
         [xx, x, @x, @@x, $x]
       end
-    ).should.be having_sexp(expected)
+    ).should.be having_sexp(expected1)
+  end
+
+  should 'handle watever(..) do ... end (as modifier)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) do
+        a = 'ia' if true
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_sexp(expected2)
+  end
+
+  should 'handle watever do ... end (as modifier)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever do
+        a = 'ia' if true
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_sexp(expected2)
+  end
+
+  should 'handle lambda do ... end (as modifier)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda do
+        a = 'ia' if true
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_sexp(expected2)
   end
 
 end
--- spec/proc_to_sexp/from_do_end_block_w_nested_unless_spec.rb
+++ spec/proc_to_sexp/from_do_end_block_w_nested_unless_spec.rb
@@ -2,12 +2,25 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe "Proc#to_sexp from do ... end block (w nested unless)" do
 
-  expected =
+  expected1 =
     s(:iter,
       s(:call, nil, :proc, s(:arglist)),
       nil,
       s(:block,
         s(:if, s(:call, nil, :aa, s(:arglist)), nil, s(:true)),
+             s(:array,
+               s(:call, nil, :xx, s(:arglist)),
+               s(:lvar, :x),
+               s(:ivar, :@x),
+               s(:cvar, :@@x),
+               s(:gvar, :$x))))
+
+  expected2 =
+    s(:iter,
+      s(:call, nil, :proc, s(:arglist)),
+      nil,
+      s(:block,
+        s(:if, s(:true), nil, s(:lasgn, :a, s(:str, "ia"))),
              s(:array,
                s(:call, nil, :xx, s(:arglist)),
                s(:lvar, :x),
@@ -22,7 +35,7 @@ describe "Proc#to_sexp from do ... end block (w nested unless)" do
         unless aa then true ; end
         [xx, x, @x, @@x, $x]
       end
-    ).should.be having_sexp(expected)
+    ).should.be having_sexp(expected1)
   end
 
   should 'handle watever do ... end' do
@@ -32,7 +45,7 @@ describe "Proc#to_sexp from do ... end block (w nested unless)" do
         unless aa then true ; end
         [xx, x, @x, @@x, $x]
       end
-    ).should.be having_sexp(expected)
+    ).should.be having_sexp(expected1)
   end
 
   should 'handle lambda do ... end' do
@@ -42,7 +55,37 @@ describe "Proc#to_sexp from do ... end block (w nested unless)" do
         unless aa then true ; end
         [xx, x, @x, @@x, $x]
       end
-    ).should.be having_sexp(expected)
+    ).should.be having_sexp(expected1)
+  end
+
+  should 'handle watever(..) do ... end (as modifier)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) do
+        a = 'ia' unless true
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_sexp(expected2)
+  end
+
+  should 'handle watever do ... end (as modifier)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever do
+        a = 'ia' unless true
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_sexp(expected2)
+  end
+
+  should 'handle lambda do ... end (as modifier)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda do
+        a = 'ia' unless true
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_sexp(expected2)
   end
 
 end
