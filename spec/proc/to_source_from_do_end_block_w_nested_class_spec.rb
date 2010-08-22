e18062636c6d2f623cde90153ae8307e19b1125b
NgTzeYang <ngty77@gmail.com>
2010-08-22 14:30:44 +0800

    Added specs for nested class.

---
+++ spec/proc/to_source_from_do_end_block_w_nested_class_spec.rb
@@ -0,0 +1,116 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+
+describe "Proc#to_source from do ... end block (w nested class)" do
+
+  expected1 = %Q\
+    proc do
+      class AA; a = "ia"; end
+      [xx, x, @x, @@x, $x]
+    end
+  \
+
+  expected2 = %Q\
+    proc do
+      class AA < Object; a = "ia"; end
+      [xx, x, @x, @@x, $x]
+    end
+  \
+
+  expected3 = %Q\
+    proc do
+      class << x; a = "ia"; end
+      [xx, x, @x, @@x, $x]
+    end
+  \
+
+  should 'handle watever(..) do ... end' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) do
+        class AA; a = 'ia'; end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever(..) do ... end (as subclass)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) do
+        class AA < Object; a = 'ia'; end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected2)
+  end
+
+  should 'handle watever(..) do ... end (as singleton)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) do
+        class << x; a = 'ia'; end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected3)
+  end
+
+  should 'handle watever do ... end' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever do
+        class AA; a = 'ia'; end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever do ... end (as subclass)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever do
+        class AA < Object; a = 'ia'; end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected2)
+  end
+
+  should 'handle watever do ... end (as singleton)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever do
+        class << x; a = 'ia'; end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected3)
+  end
+
+  should 'handle lambda do ... end' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda do
+        class AA; a = 'ia'; end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle lambda do ... end (as subclass)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda do
+        class AA < Object; a = 'ia'; end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected2)
+  end
+
+  should 'handle lambda do ... end (as singleton)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda do
+        class << x; a = 'ia'; end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected3)
+  end
+
+end
