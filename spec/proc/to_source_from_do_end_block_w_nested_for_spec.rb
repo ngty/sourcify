c2684d812e885f64836e16111ac6e831b42711eb
NgTzeYang <ngty77@gmail.com>
2010-08-22 13:46:47 +0800

    Added specs for 'for' block.

---
+++ spec/proc/to_source_from_do_end_block_w_nested_for_spec.rb
@@ -0,0 +1,145 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+
+describe "Proc#to_source from do ... end block (w nested for block)" do
+
+  expected1 = %|proc {\n for i in [1,2] do a = "ia" end\n  [xx, x, @x, @@x, $x] }|
+
+  should 'handle watever(..) do ... end (w do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) do
+        for i in [1,2] do a = "ia" end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever(..) do ... end (wo do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) do
+        for i in [1,2]
+          a = 'ia'
+        end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever(..) \ do ... end (w do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) \
+        do
+          for i in [1,2] do a = "ia" end
+          [xx, x, @x, @@x, $x]
+        end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever(..) \ do ... end (wo do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever(:a, :b, {:c => 1}) \
+        do
+          for i in [1,2]
+            a = 'ia'
+          end
+          [xx, x, @x, @@x, $x]
+        end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever do ... end (w do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever do
+        for i in [1,2] do a = "ia" end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever do ... end (wo do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever do
+        for i in [1,2]
+          a = 'ia'
+        end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever \ do ... end (w do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever \
+        do
+          for i in [1,2] do a = "ia" end
+          [xx, x, @x, @@x, $x]
+        end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle watever \ do ... end (wo do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      watever \
+        do
+          for i in [1,2]
+            a = 'ia'
+          end
+          [xx, x, @x, @@x, $x]
+        end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle lambda do ... end (w do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda do
+        for i in [1,2] do a = "ia" end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle lambda do ... end (wo do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda do
+        for i in [1,2]
+          a = 'ia'
+        end
+        [xx, x, @x, @@x, $x]
+      end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle lambda \ do ... end (w do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda \
+        do
+          for i in [1,2] do a = "ia" end
+          [xx, x, @x, @@x, $x]
+        end
+    ).should.be having_same_code_as(expected1)
+  end
+
+  should 'handle lambda \ do ... end (wo do)' do
+    x, @x, @@x, $x = 'lx', 'ix', 'cx', 'gx'
+    (
+      lambda \
+        do
+          for i in [1,2]
+            a = 'ia'
+          end
+          [xx, x, @x, @@x, $x]
+        end
+    ).should.be having_same_code_as(expected1)
+  end
+
+end
