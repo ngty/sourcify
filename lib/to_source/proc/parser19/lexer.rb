888cb3479a636fd4d0c3fd8872591984eec9917c
NgTzeYang <ngty77@gmail.com>
2010-08-25 05:44:11 +0800

    Heavy cleanup to avoid using the case ... end statement.

--- lib/to_source/proc/parser19/lexer.rb
+++ lib/to_source/proc/parser19/lexer.rb
@@ -22,90 +22,85 @@ module ToSource
 
         def on_kw(token)
           super.tap do |rs|
+            send(:"on_kw_#{token}", rs) rescue NoMethodError
+          end
+        end
+
+        def on_kw_class(rs)
+          # Pretty straightforward for these, each of them will consume an 'end' close it
+          @do_end_counter.increment_start if @do_end_counter.started?
+        end
+
+        # These work the same as 'class', the exception is 'for', which can have an optional
+        # 'do' attached:
+        # * for a in [1,2] do ... end
+        # * for a in [1,2] \n ... end
+        %w{def module begin case for}.each{|kw| alias_method :"on_kw_#{kw}", :on_kw_class }
+
+        def on_kw_while(rs)
+          # This has optional trailing 'do', and can work as a modifier as well, eg:
+          # * while true do ... end # => 'do' must be on the same line as 'while'
+          # * while true \n ... end
+          # * ... while true # => 'while' is pre-pended with non-spaces
+          if @do_end_counter.started? && (rs.start_of_line? or rs.within_block?)
+            @do_end_counter.increment_start
+          end
+        end
+
+        # These work exactly the same as 'while'.
+        %w{until if unless}.each{|kw| alias_method :"on_kw_#{kw}", :on_kw_while }
+
+        def on_kw_do(rs)
+          if !@do_end_counter.started?
             rs.extend(Extensions::Result) unless rs.respond_to?(:curr)
+            @do_end_counter.marker = rs.curr
+            @do_end_counter.increment_start
+          elsif rs.same_as_curr_line.keywords(%w{for while until}).empty?
+            # It is possible for a 'for', 'while' or 'until' to have an attached 'do',
+            # for such a case, we want to skip it
+            @do_end_counter.increment_start
+          end
+        end
 
-            case token
+        def on_kw_end(rs)
+          if @do_end_counter.started? && @do_end_counter.increment_end.telly?
+            @result = rs.to_code(@do_end_counter.marker)
+            raise EndOfBlock
+          end
+        end
 
-            when 'class', 'def', 'module', 'begin', 'case'
-              # Pretty straightforward for these, each of them will consume an 'end' close it
-              @do_end_counter.increment_start if @do_end_counter.started?
-
-            when 'for'
-              # This has an optional trailing 'do', eg:
-              # * for a in [1,2] do ... end
-              # * for a in [1,2] \n ... end
-              @do_end_counter.increment_start if @do_end_counter.started?
-
-            when 'while', 'until', 'if', 'unless'
-              # These have optional trailing 'do' (only for 'while' & 'until'), and can work
-              # as a modifier as well, eg:
-              # * while true do ... end # => 'do' must be on the same line as 'while'
-              # * while true \n ... end
-              # * ... while true # => 'while' is pre-pended with non-spaces
-              if @do_end_counter.started?
-                if rs.start_of_line? or rs.within_block?
-                  @do_end_counter.increment_start
-                end
-              end
-
-            when 'do'
-              if !@do_end_counter.started?
-                # The 1st 'do' on the line will mark the start of the proc (which may not
-                # be true, but anyway, just do it for now, KISS !!
-                @do_end_counter.marker = rs.curr
-                @do_end_counter.increment_start
-
-              elsif rs.same_as_curr_line.keywords(%w{for while until}).empty?
-                # It is possible for a 'for', 'while' or 'until' to have an attached 'do',
-                # for such a case, we want to skip it
-                @do_end_counter.increment_start
-              end
-
-            when 'end'
-              # Very straigtforward, every 'end' will be considered !!
-              if @do_end_counter.started?
-                if @do_end_counter.increment_end.telly?
-                  @result = rs.to_code(@do_end_counter.marker)
-                  raise EndOfBlock
-                end
-              end
-
+        def on_lbrace(token)
+          super.tap do |rs|
+            unless @do_end_counter.started?
+              rs.extend(Extensions::Result) unless rs.respond_to?(:curr)
+              @braced_counter.marker = rs.curr unless @braced_counter.started?
+              @braced_counter.increment_start
             end
           end
         end
 
         def on_rbrace(token)
           super.tap do |rs|
-            break if @do_end_counter.started? or !@braced_counter.started?
-            if @braced_counter.increment_end.telly?
-              rs.extend(Extensions::Result) unless rs.respond_to?(:curr)
+            if @braced_counter.started? && @braced_counter.increment_end.telly?
               @result = rs.to_code(@braced_counter.marker)
               raise EndOfBlock
             end
           end
         end
 
-        def on_lbrace(token)
-          super.tap do |rs|
-            break if @do_end_counter.started?
-            rs.extend(Extensions::Result) unless rs.respond_to?(:curr)
-            @braced_counter.marker = rs.curr unless @braced_counter.started?
-            @braced_counter.increment_start
-          end
-        end
-
         def on_op(token)
           super.tap do |rs|
-            break if @do_end_counter.started? or !@braced_counter.started?
-            @braced_counter.decrement_start if token == '=>' && @braced_counter[:start] == 1
+            if @braced_counter.started? && token == '=>' && @braced_counter[:start] == 1
+              @braced_counter.decrement_start
+            end
           end
         end
 
         def on_label(token)
           super.tap do |rs|
-            rs.extend(Extensions::Result) unless rs.respond_to?(:curr)
-            break if @do_end_counter.started? or !@braced_counter.started?
-            @braced_counter.decrement_start if @braced_counter[:start] == 1
+            if @braced_counter.started? && @braced_counter[:start] == 1
+              @braced_counter.decrement_start
+            end
           end
         end
 
