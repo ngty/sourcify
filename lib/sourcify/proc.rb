c10babd00be67bc03c8d3484d375e1eb5b28fcff
NgTzeYang <ngty77@gmail.com>
2010-08-28 11:28:57 +0800

    Fixed missing Proc#source_location under 1.8.* (where ParseTree is available).

--- lib/sourcify/proc.rb
+++ lib/sourcify/proc.rb
@@ -3,12 +3,26 @@ module Sourcify
 
     def self.included(base)
       base.class_eval do
+
+        meths = instance_methods.map(&:to_s)
+
+        # Added as a bonus, by default, only 1.9.* implements this.
+        unless meths.include?('source_location')
+          def source_location
+            @source_location ||= (
+              file, line = /^#<Proc:0x[0-9A-Fa-f]+@(.+):(\d+).*?>$/.match(inspect)[1..2]
+              [file, line.to_i]
+            )
+          end
+        end
+
+        # When ParseTree is available, we just make use of all the convenience it offers :)
         if Object.const_defined?(:ParseTree)
-          # When ParseTree is available, we just make use of all the convenience it offers :)
+
           alias_method :to_source, :to_ruby
+
+        # Otherwise, we are going to do abit of static text parsing :(
         else
-          # Otherwise, we are going to do abit of static text parsing :(
-          meths = instance_methods.map(&:to_s)
 
           unless meths.include?('to_source')
             def to_source
@@ -21,15 +35,6 @@ module Sourcify
             def to_sexp
               require 'sourcify/proc/parser'
               (@parser ||= Parser.new(self)).sexp
-            end
-          end
-
-          unless meths.include?('source_location')
-            def source_location
-              @source_location ||= (
-                file, line = /^#<Proc:0x[0-9A-Fa-f]+@(.+):(\d+).*?>$/.match(inspect)[1..2]
-                [file, line.to_i]
-              )
             end
           end
 
