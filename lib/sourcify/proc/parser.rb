cc767c31af28d257d85228c8ff3c6a0d357a70b9
NgTzeYang <ngty77@gmail.com>
2010-09-08 14:02:48 +0800

    Cleaning up Sourcify::Proc::Parser.

--- lib/sourcify/proc/parser.rb
+++ lib/sourcify/proc/parser.rb
@@ -20,73 +20,97 @@ module Sourcify
       def sexp
         @sexp ||= (
           raw_sexp = RUBY_PARSER.parse(raw_source, @source_code.file)
-          Sexp.from_array(replace_with_lvars(raw_sexp.to_a))
+          Sexp.from_array(Normalizer.process(raw_sexp, @binding))
         )
       end
 
       private
 
         def raw_source
-          @raw_source ||=
-            if (frags = raw_source_frags).size > 1
-              raise MultipleMatchingProcsPerLineError
-            else
-              # Cheapo hack to ensure __LINE__ is correctly represented !!
-              ("\n" * @source_code.line.pred) + frags[0]
-            end
-        end
-
-        def raw_source_frags
-          begin
-            rscan_for_proc_frags(@source_code.to_s, false).flatten.
-              select{|code| eval(code).arity == @arity }
-          rescue Exception
-            raise ParserInternalError
-          end
-        end
-
-        def rscan_for_proc_frags(str, stop_on_newline)
-          (Scanner.process(str, stop_on_newline) || []).map do |outer|
-            inner = rscan_for_proc_frags(outer.sub(/^proc\s*(do|\{)/,''), true)
-            [outer, inner]
-          end
-        end
-
-        def replace_with_lvars(array)
-          return array if [:class, :sclass, :defn, :module].include?(array[0])
-          array.map do |e|
-            if e.is_a?(Array)
-              no_arg_method_call_or_lvar(e) or replace_with_lvars(e)
-            else
-              e
+          CodeScanner.process(@source_code) do |code|
+            begin
+              eval(code).arity == @arity
+            rescue Exception
+              raise ParserInternalError
             end
           end
         end
 
-        def no_arg_method_call_or_lvar(e)
-          if represents_no_arg_call?(e)
-            has_as_local_var?(var = e[2]) ? [:lvar, var] : e
+        class Normalizer
+          class << self
+
+            def process(sexp, binding)
+              @binding = binding
+              fix_no_arg_method_calls(sexp.to_a)
+            end
+
+            def fix_no_arg_method_calls(array)
+              return array if [:class, :sclass, :defn, :module].include?(array[0])
+              array.map do |e|
+                if e.is_a?(Array)
+                  no_arg_method_call?(e) or fix_no_arg_method_calls(e)
+                else
+                  e
+                end
+              end
+            end
+
+            def no_arg_method_call?(e)
+              if like_no_arg_method_call?(e)
+                bounded_var?(var = e[2]) ? [:lvar, var] : e
+              end
+            end
+
+            def like_no_arg_method_call?(e)
+              e.size == 4 && e[0..1] == [:call, nil] &&
+                e[3] == [:arglist] && (var = e[2]).is_a?(Symbol)
+            end
+
+            def bounded_var?(var)
+              qvar = (@q ||= (RUBY_VERSION.include?('1.9.') ? ":%s" : "'%s'")) % var
+              @binding.eval("local_variables.include?(#{qvar})")
+            end
+
           end
         end
 
-        def represents_no_arg_call?(e)
-          e.size == 4 && e[0..1] == [:call, nil] &&
-            e[3] == [:arglist] && (var = e[2]).is_a?(Symbol)
-        end
+        class CodeScanner
+          class << self
 
-        def has_as_local_var?(var)
-          qvar = (@q ||= (RUBY_VERSION.include?('1.9.') ? ":%s" : "'%s'")) % var
-          @binding.eval("local_variables.include?(#{qvar})")
+            def process(source_code, &matcher)
+              result = rscan(source_code.to_s, false).flatten.select(&matcher)
+              if result.size > 1
+                raise MultipleMatchingProcsPerLineError
+              else
+                # Cheapo hack to ensure __LINE__ is correctly represented !!
+                ("\n" * source_code.line) + result[0]
+              end
+            end
+
+            def rscan(str, stop_on_newline)
+              (Scanner.process(str, stop_on_newline) || []).map do |outer|
+                inner = rscan(outer.sub(/^proc\s*(do|\{)/,''), true)
+                [outer, inner]
+              end
+            end
+
+          end
         end
 
         class SourceCode < Struct.new(:file, :line)
+
+          def line
+            super.pred
+          end
+
           def to_s
             File.open(file, 'r') do |fh|
               fh.extend(File::Tail)
-              fh.forward(line.pred)
+              fh.forward(line)
               fh.readlines.join
             end
           end
+
         end
 
     end
