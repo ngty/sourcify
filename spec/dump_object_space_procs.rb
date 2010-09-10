70f8249b7d4dad7c32ddfda1b05de27b16301d4a
NgTzeYang <ngty77@gmail.com>
2010-09-11 00:22:05 +0800

    Cleaning up, less hackish, slightly more organized.

--- spec/dump_object_space_procs.rb
+++ spec/dump_object_space_procs.rb
@@ -8,9 +8,11 @@ if RUBY_PLATFORM =~ /java/i
   reload! rescue nil
 end
 
-def dump_object_space_procs(debug = false)
-  errors = []
+DUMPED_OBJECT_SPACE_PROCS = {:max => 800, :done => 0}
+DUMP_OBJECT_SPACE_PROCS_ERRORS = []
+class STOP_DUMPING_OBJECT_SPACE_PROCS < Exception; end
 
+def dump_object_space_procs(debug = false)
   # Determine working dir
   name = [
     RUBY_DESCRIPTION =~ /enterprise/i ? 'ree' : (RUBY_PLATFORM =~ /java/i ? 'jruby' : 'mri'),
@@ -24,44 +26,59 @@ def dump_object_space_procs(debug = false)
     '== NOTE: dump files can be found at %s' % dump_dir
 
   # Core processing
-  results = Benchmark.measure do
-    ObjectSpace.each_object(Proc).to_a.
-      group_by{|o| o.source_location[0] }.each do |ofile, objs|
-        nfile = File.join(dump_dir, ofile.gsub('/','~'))
-        File.open(nfile,'w') do |f|
-          objs.sort_by{|o| o.source_location[1] }.map do |o|
-            begin
-              data = {
-                :location => o.source_location,
-                :sexp => o.to_sexp,
-                :source => o.to_source
-              }
-              f.puts(data.pretty_inspect)
-              print '.'
-            rescue Exception
-              data = {
-                :location => o.source_location,
-                :error => $!.inspect
-              }
-              errors << data
-              f.puts(data.pretty_inspect)
-              pp(data) if debug
-              print 'x'
-            end
+  ObjectSpace.each_object(Proc).to_a.
+    group_by{|o| o.source_location[0] }.each do |ofile, objs|
+      nfile = File.join(dump_dir, ofile.gsub('/','~'))
+      File.open(nfile,'w') do |f|
+        objs.sort_by{|o| o.source_location[1] }.map do |o|
+          DUMPED_OBJECT_SPACE_PROCS[:done] += 1
+          begin
+            data = {
+              :location => o.source_location,
+              :sexp => o.to_sexp,
+              :source => o.to_source
+            }
+            f.puts(data.pretty_inspect)
+            print '.'
+          rescue Exception
+            data = {
+              :location => o.source_location,
+              :error => $!.inspect
+            }
+            DUMP_OBJECT_SPACE_PROCS_ERRORS << data
+            f.puts(data.pretty_inspect)
+            pp(data) if debug
+            print 'x'
+          ensure
+            raise STOP_DUMPING_OBJECT_SPACE_PROCS \
+              if DUMPED_OBJECT_SPACE_PROCS.values.uniq.size == 1
           end
         end
       end
-  end
-
-  puts '',''
-  unless errors.empty?
-    puts '== OOPS, we have some erorrs :('
-    errors.each_with_index{|e, i| print "#{i}). %s" % e.pretty_inspect }
-  else
-    puts '== YEAH, no errors :)'
-  end
-  puts '', '== Benchmark results:',
-    results.to_s, ''
+    end
 end
 
-dump_object_space_procs
+# Dumping in action
+figures = Benchmark.measure do
+  begin
+    dump_object_space_procs
+  rescue STOP_DUMPING_OBJECT_SPACE_PROCS
+  end
+end
+
+# Printout the failures/passes
+unless DUMP_OBJECT_SPACE_PROCS_ERRORS.empty?
+  puts '', '== OOPS, we have some erorrs :('
+  DUMP_OBJECT_SPACE_PROCS_ERRORS.each_with_index do |e, i|
+    print "#{i}). %s" % e.pretty_inspect
+  end
+else
+  puts '', '== YEAH, no errors :)'
+end
+
+# Printout the benchmark results
+puts '',
+  '== Benchmark results for processing %s procs:' % DUMPED_OBJECT_SPACE_PROCS[:done],
+  figures.to_s, ''
+
+# __END__
