71a561a7202c7cb7da0efa5f5d677b6b3a946a33
NgTzeYang <ngty77@gmail.com>
2010-09-23 23:14:38 +0800

    More readable examples.

--- spec/proc/to_source_within_irb_spec.rb
+++ spec/proc/to_source_within_irb_spec.rb
@@ -2,25 +2,37 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe "Proc#to_source within IRB" do
 
+  class << self
+
+    def irb_eval(string)
+      irb_exec(string)[-1]
+    end
+
+    def equal_to(expected)
+      lambda {|found| normalize_code(found) == normalize_code(expected) }
+    end
+
+  end
+
   should 'handle basic' do
-    normalize_code(irb_exec(%Q(
+    irb_eval(%Q(
       x = lambda { 1+2 }
       y = lambda { 2+3 }
       z = lambda { 3+4 }
       y.to_source
-    ))[-1]).should.equal normalize_code('proc { 2+3 }')
+    )).should.be equal_to('proc { 2+3 }')
   end
 
   should 'handle magic variable __FILE__' do
-    normalize_code(irb_exec(%Q(
+    irb_eval(%Q(
       lambda { __FILE__ }.to_source
-    ))[-1]).should.equal normalize_code('proc { "(irb)" }')
+    )).should.be equal_to('proc { "(irb)" }')
   end
 
   should 'handle magic variable __LINE__' do
-    normalize_code(irb_exec(%Q(
+    irb_eval(%Q(
       lambda { __LINE__ }.to_source
-    ))[-1]).should.equal normalize_code('proc { 2 }')
+    )).should.be equal_to('proc { 2 }')
   end
 
 end
