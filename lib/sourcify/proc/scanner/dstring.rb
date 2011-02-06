68c25116a86a3872ba05693616ecc7764de839b6
Ng Tze Yang <ngty77@gmail.com>
2011-02-06 13:38:15 +0800

    Use RubyParser to determine validity of string.

--- lib/sourcify/proc/scanner/dstring.rb
+++ lib/sourcify/proc/scanner/dstring.rb
@@ -16,16 +16,7 @@ module Sourcify
         end
 
         def closed?
-          # NOTE: The only real error is SyntaxError, other errors are due
-          # to undefined variable or watever, which are perfectly ok when
-          # testing for validity of the string.
-          begin
-            instance_eval(safe_contents) if evaluable?
-          rescue SyntaxError
-            false
-          rescue Exception
-            true
-          end
+          evaluable? && parsable?
         end
 
         private
@@ -36,10 +27,23 @@ module Sourcify
             @contents[-1][-1].chr == end_tag
           end
 
+          def parsable?
+            begin
+              RubyParser.new.parse(<<-SOURCIFIED_HEREDOKIE
+                  #{safe_contents}
+                SOURCIFIED_HEREDOKIE
+              ) && true
+            rescue Racc::ParseError, SyntaxError
+              false
+            rescue Exception
+              true
+            end
+          end
+
           def safe_contents
             # NOTE: %x & ` strings are dangerous to eval cos they execute shell commands,
             # thus we convert them to normal strings 1st
-            @contents.join.gsub(/(%x)(\W|\_)/, '%Q\2').gsub(/.{0,2}(`)/) do |s|
+            to_s.gsub(/(%x)(\W|\_)/, '%Q\2').gsub(/.{0,2}(`)/) do |s|
               s =~ /^(%Q|%W|%r|%x|.?%|.?\\)/ ? s : s.sub(/`$/,'%Q`')
             end
           end
