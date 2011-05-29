20cd6dff9db8b39126c460b10ca35637317a15d2
NgTzeYang <ngty77@gmail.com>
2011-05-29 16:13:56 +0800

    Converter & SourceCode are really generic.

--- lib/sourcify/proc/parser/converter.rb
+++ lib/sourcify/proc/parser/converter.rb
@@ -1,29 +1,8 @@
 module Sourcify
   module Proc
     class Parser #:nodoc:all
-      class Converter
-        class << self
-
-          RUBY_PARSER = RubyParser.new
-          RUBY_2_RUBY = Ruby2Ruby.new
-
-          def to_sexp(code, file = nil)
-            retried = false
-            begin
-              RUBY_PARSER.reset
-              (retried ? RubyParser.new : RUBY_PARSER).parse(*[code, file].compact)
-            rescue Racc::ParseError, SyntaxError
-              return nil if retried
-              retried = true; retry
-            end
-          end
-
-          def to_code(sexp)
-            RUBY_2_RUBY.process(Sexp.from_array(sexp.to_a))
-          end
-
-        end
-      end
+      Sourcify.require_rb('common', 'parser', 'converter')
+      Converter = Common::Parser::Converter
     end
   end
 end
