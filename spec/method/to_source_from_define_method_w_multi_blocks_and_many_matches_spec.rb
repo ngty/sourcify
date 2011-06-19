45fe78e6934dc63483c0c4edee8a4568c48b2c4d
NgTzeYang <ngty77@gmail.com>
2011-06-19 23:54:00 +0800

    More thorough specs to investigate failure to raise ProbablyDefinedByProc.

--- spec/method/to_source_from_define_method_w_multi_blocks_and_many_matches_spec.rb
+++ spec/method/to_source_from_define_method_w_multi_blocks_and_many_matches_spec.rb
@@ -10,22 +10,44 @@ describe 'Method#to_source from #define_method w multi blocks and many matches' 
 
     error = Sourcify::MultipleMatchingProcsPerLineError
 
-    should "raise #{error} w no nesting on same line" do
-      b1 = lambda {|a| @x }; b2 = lambda { @x }; b3 = lambda { @x }
-      @thing.class.send(:define_method, :m1, &b2)
-      lambda { @thing.method(:m1).to_source }.should.raise(error)
+    describe 'w do...end block' do
+      should "raise #{error} w no nesting on same line" do
+        b1 = lambda do |a| @x end; b2 = lambda do @x end; b3 = lambda do @x end
+        @thing.class.send(:define_method, :m1, &b2)
+        lambda { @thing.method(:m1).to_source }.should.raise(error)
+      end
+
+      should "raise #{error} w single level nesting on same line" do
+        b1 = lambda do |a| @x end; b2 = lambda do lambda do @x end end
+        @thing.class.send(:define_method, :m2, &b2)
+        lambda { @thing.method(:m2).to_source }.should.raise(error)
+      end
+
+      should "raise #{error} w multi level nesting on same line" do
+        b2 = (lambda do |a| lambda do  lambda do  @x end end end).call(1)
+        @thing.class.send(:define_method, :m3, &b2)
+        lambda { @thing.method(:m3).to_source }.should.raise(error)
+      end
     end
 
-    should "raise #{error} w single level nesting on same line" do
-      b1 = lambda {|a| @x }; b2 = lambda { lambda { @x } }
-      @thing.class.send(:define_method, :m2, &b2)
-      lambda { @thing.method(:m2).to_source }.should.raise(error)
-    end
+    describe 'w braced block' do
+      should "raise #{error} w no nesting on same line" do
+        b1 = lambda {|a| @x }; b2 = lambda { @x }; b3 = lambda { @x }
+        @thing.class.send(:define_method, :m1, &b2)
+        lambda { @thing.method(:m1).to_source }.should.raise(error)
+      end
 
-    should "raise #{error} w multi level nesting on same line" do
-      b2 = (lambda {|a| lambda { lambda { @x } } }).call(1)
-      @thing.class.send(:define_method, :m3, &b2)
-      lambda { @thing.method(:m3).to_source }.should.raise(error)
+      should "raise #{error} w single level nesting on same line" do
+        b1 = lambda {|a| @x }; b2 = lambda { lambda { @x } }
+        @thing.class.send(:define_method, :m2, &b2)
+        lambda { @thing.method(:m2).to_source }.should.raise(error)
+      end
+
+      should "raise #{error} w multi level nesting on same line" do
+        b2 = (lambda {|a| lambda { lambda { @x } } }).call(1)
+        @thing.class.send(:define_method, :m3, &b2)
+        lambda { @thing.method(:m3).to_source }.should.raise(error)
+      end
     end
 
   end
--- spec/method/to_source_from_define_method_w_multi_blocks_and_single_match_spec.rb
+++ spec/method/to_source_from_define_method_w_multi_blocks_and_single_match_spec.rb
@@ -6,36 +6,68 @@ describe 'Method#to_source from #define_method w single blocks and many matches'
     @thing = Object.new
   end
 
-  should 'skip non-matching w no nesting on same line' do
-    puts 'TODO: fix broken spec in %s' % __FILE__
-    true.should == true
-#    b1 = lambda do |a| @x1 end; b2 = lambda do |b| @x2 end
-#    @thing.class.send(:define_method, :m1, &b2)
-#    @thing.method(:m1).should.be having_source(%(
-#      def m1(b)
-#        @x2
-#      end
-#    ))
+  describe 'w do...end block' do
+    should 'skip non-matching w no nesting on same line' do
+      b1 = lambda do |a| @x1 end; b2 = lambda do |b| @x2 end
+      @thing.class.send(:define_method, :m1, &b2)
+      @thing.method(:m1).should.be having_source(%(
+        def m1(b)
+          @x2
+        end
+      ))
+    end
+
+    should 'skip non-matching w single level nesting on same line' do
+      b2 = (lambda do |a| lambda do |b| @x2 end end).call(1)
+      @thing.class.send(:define_method, :m2, &b2)
+      @thing.method(:m2).should.be having_source(%(
+        def m2(b)
+          @x2
+        end
+      ))
+    end
+
+    should 'skip non-matching w multi level nesting on same line' do
+      b2 = (lambda do |a| lambda do |b| b2 = lambda do |c| @x3 end end end).call(1).call(1)
+      @thing.class.send(:define_method, :m3, &b2)
+      @thing.method(:m3).should.be having_source(%(
+        def m3(c)
+          @x3
+        end
+      ))
+    end
   end
 
-  should 'skip non-matching w single level nesting on same line' do
-    b2 = (lambda {|a| lambda {|b| @x2 } }).call(1)
-    @thing.class.send(:define_method, :m2, &b2)
-    @thing.method(:m2).should.be having_source(%(
-      def m2(b)
-        @x2
-      end
-    ))
-  end
+  describe 'w braced block' do
+    should 'skip non-matching w no nesting on same line' do
+      b1 = lambda {|a| @x1 }; b2 = lambda {|b| @x2 }
+      @thing.class.send(:define_method, :m1, &b2)
+      @thing.method(:m1).should.be having_source(%(
+        def m1(b)
+          @x2
+        end
+      ))
+    end
 
-  should 'skip non-matching w multi level nesting on same line' do
-    b2 = (lambda {|a| lambda {|b| b2 = lambda {|c| @x3 } } }).call(1).call(1)
-    @thing.class.send(:define_method, :m3, &b2)
-    @thing.method(:m3).should.be having_source(%(
-      def m3(c)
-        @x3
-      end
-    ))
+    should 'skip non-matching w single level nesting on same line' do
+      b2 = (lambda {|a| lambda {|b| @x2 } }).call(1)
+      @thing.class.send(:define_method, :m2, &b2)
+      @thing.method(:m2).should.be having_source(%(
+        def m2(b)
+          @x2
+        end
+      ))
+    end
+
+    should 'skip non-matching w multi level nesting on same line' do
+      b2 = (lambda {|a| lambda {|b| b2 = lambda {|c| @x3 } } }).call(1).call(1)
+      @thing.class.send(:define_method, :m3, &b2)
+      @thing.method(:m3).should.be having_source(%(
+        def m3(c)
+          @x3
+        end
+      ))
+    end
   end
 
 end
