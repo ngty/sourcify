96dbc0f098584b39c12ff3b3eb1412030626eb12
NgTzeYang <ngty77@gmail.com>
2011-07-30 00:26:29 +0800

    Only rubies that has Method#source_location will run specs under spec/method.

--- spec/run_spec.sh
+++ spec/run_spec.sh
@@ -3,4 +3,12 @@
 echo ''
 echo `gem env | grep 'INSTALLATION DIRECTORY' | sed 's/.*\/\(.*\)/\1:/'`
 rm -rf ~/.ruby_inline/*ParseTree*
-bacon spec/*/*/*_spec.rb spec/*/*_spec.rb
+
+SKIPPABLE=`ruby -e '"".method(:to_i).source_location rescue puts "oops"'`
+
+if [ -n $SKIPPABLE ]; then
+  echo "NOTE: Method#to_source (& friends) not supported in this ruby !!"
+  bacon spec/proc/*/*_spec.rb spec/proc/*_spec.rb
+else
+  bacon spec/*/*/*_spec.rb spec/*/*_spec.rb
+fi
