118e0be315c2036d0d5ea33ab1a494e19493536a
NgTzeYang <ngty77@gmail.com>
2011-07-30 00:34:26 +0800

    Fine-tune message displayed & condition testing.

--- spec/run_spec.sh
+++ spec/run_spec.sh
@@ -6,9 +6,9 @@ rm -rf ~/.ruby_inline/*ParseTree*
 
 SKIPPABLE=`ruby -e '"".method(:to_i).source_location rescue puts "oops"'`
 
-if [ -n $SKIPPABLE ]; then
-  echo "NOTE: Method#to_source (& friends) not supported in this ruby !!"
-  bacon spec/proc/*/*_spec.rb spec/proc/*_spec.rb
-else
+if [ -z "$SKIPPABLE" ]; then
   bacon spec/*/*/*_spec.rb spec/*/*_spec.rb
+else
+  echo "NOTE: This ruby doesn't support Method#to_source (& friends) !!"
+  bacon spec/proc/*/*_spec.rb spec/proc/*_spec.rb
 fi
