bc71329b1ff5836406a3bb8a93415c3810aa61bd
NgTzeYang <ngty@gmail.com>
2012-02-25 17:33:25 +0000

    Added support for auto-generating of examples based on fixtures.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,50 @@
 require 'rubygems'
 require 'minitest/spec'
 require File.expand_path('../../lib/sourcify', __FILE__)
+
+
+module Sourcify
+  module SpecHelper
+
+    class << self
+      attr_accessor :fixtures
+
+      def reset_fixtures!
+        self.fixtures = []
+      end
+
+      def add_fixture(description_and_expectation, block)
+        lines = description_and_expectation.split("\n")
+
+        fixtures << [
+          lines.grep(/^#!! /).join("\n").gsub('#!! ','').strip,
+          lines.grep(/^#>> /).join("\n").gsub('#>> ','').strip,
+          block
+        ]
+      end
+
+      def each_fixture
+        fixtures.each {|*args| yield(*args) }
+      end
+    end
+
+    def examplify_fixtures(name)
+      SpecHelper.reset_fixtures!
+
+      klass, type = "#{name}".split('/')
+      load File.expand_path("../#{klass}/fixtures/#{type}.rb", __FILE__)
+
+      SpecHelper.each_fixture do |description, expected, block|
+        it description do
+          block.to_source.must_equal(expected)
+        end
+      end
+    end
+
+  end
+end
+
+
+def f(description_and_expectation, block)
+  Sourcify::SpecHelper.add_fixture(description_and_expectation, block)
+end
