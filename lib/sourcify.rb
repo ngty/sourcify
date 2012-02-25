022fa02b23d8c64c0f9f9f37d9782ae2d0453c1a
NgTzeYang <ngty@gmail.com>
2012-02-25 17:31:33 +0000

    Bullet-proof requiring of lib files.

--- lib/sourcify.rb
+++ lib/sourcify.rb
@@ -1,5 +1,10 @@
-require "sourcify/version"
-
 module Sourcify
-  # Your code goes here...
+  class << self
+    def require_rb(*files)
+      @root ||= File.expand_path('../sourcify', __FILE__)
+      [files].flatten.each{|f| require File.join(@root, f) }
+    end
+  end
 end
+
+Sourcify.require_rb(%w{version})
