5cc9817d302923bf0cd8a7f098cdc4cc83a1db55
NgTzeYang <ngty@gmail.com>
2012-02-27 04:49:02 +0000

    Revised notation to specify description & expected source, & added   support for describing error condition.

--- spec/fixtures/proc-do_end_block.rb
+++ spec/fixtures/proc-do_end_block.rb
@@ -1,22 +1,26 @@
 # -*- encoding: utf-8 -*-
 
-f("
-#!! wo argument
-#>> lambda do
-#>>   x + 1
-#>> end",
+f(%^
+## wo argument
+##
+#> lambda do
+#>   x + 1
+#> end
+^,(
 lambda do
   x + 1
 end
-)
+))
 
-f("
-#!! w 1 argument
-#>> lambda do |x|
-#>>   x + 1
-#>> end",
+f(%^
+## w 1 argument
+##
+#> lambda do |x|
+#>   x + 1
+#> end
+^,(
 lambda do |x|
   x + 1
 end
-)
+))
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,27 +5,51 @@ require File.expand_path('../../lib/sourcify', __FILE__)
 
 module Sourcify
   module SpecHelper
-
     class << self
+
       attr_accessor :fixtures
 
       def reset_fixtures!
         self.fixtures = []
       end
 
-      def add_fixture(description_and_expectation, block)
-        lines = description_and_expectation.split("\n")
-
-        fixtures << [
-          lines.grep(/^#!! /).join("\n").gsub('#!! ','').strip,
-          lines.grep(/^#>> /).join("\n").gsub('#>> ','').strip,
-          block
-        ]
+      def add_fixture(paragraph, block)
+        description, expected = extract_args(paragraph)
+        fixtures << [description, expected, block]
       end
 
       def each_fixture
         fixtures.each {|*args| yield(*args) }
       end
+
+    private
+
+      def extract_args(paragraph)
+        lines = paragraph.split("\n")
+
+        description, source, error = %w{## #> #!}.map do |pfx|
+          pattern = /^#{pfx} /
+          lines.grep(pattern).map{|s| s.sub(pattern,'') }.join("\n")
+        end
+
+        unless error.empty?
+          def error.to_constant
+            Sourcify::SpecHelper.send(:constantize, self)
+          end
+        end
+
+        [
+          description,
+          {:source => source, :error => error}.reject{|k,v| v.empty? }
+        ]
+      end
+
+      def constantize(string)
+        string.split('::').inject(Object) do |klass, constant|
+          klass.const_get(constant)
+        end
+      end
+
     end
 
     def examplify_fixtures(name)
@@ -33,16 +57,30 @@ module Sourcify
       load File.expand_path("../fixtures/#{name}.rb", __FILE__)
 
       SpecHelper.each_fixture do |description, expected, block|
-        it description do
-          block.to_source.must_equal(expected)
+        expected.each do |type, val|
+          send(:"example_for_#{type}", description, val, block)
         end
+      end
+    end
+
+  private
+
+    def example_for_source(description, expected, block)
+      it description do
+        block.to_source.must_equal(expected)
+      end
+    end
+
+    def example_for_error(description, expected, block)
+      it description do
+        lambda { block.to_source }.must_raise(expected.to_constant)
       end
     end
 
   end
 end
 
-
-def f(description_and_expectation, block)
-  Sourcify::SpecHelper.add_fixture(description_and_expectation, block)
+def f(paragraph, block)
+  Sourcify::SpecHelper.add_fixture(paragraph, block)
 end
+
