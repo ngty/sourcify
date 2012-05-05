190607a24e595eec8411e023b88ee9ca78303bb9
Ng Tze Yang <ngty77@gmail.com>
2012-05-06 01:59:25 +0800

    Added support for extracting raw source.

--- lib/sourcify/proc/extractor.rb
+++ lib/sourcify/proc/extractor.rb
@@ -9,6 +9,19 @@ module Sourcify
           Sorcerer.source(sexp)
         end
 
+        def extract_raw_source(block)
+          file, line = block.source_location
+          positions = SexpUtil.new(::File.read(file)).locate(:line => line)
+          normalize_indents(File.chunk(file, positions[:from], positions[:till]))
+        end
+
+      private
+
+        def normalize_indents(code)
+          indent = code.split("\n")[-1].match(/^\s+/)
+          code.gsub("\n#{indent}","\n")
+        end
+
       end
     end
   end
