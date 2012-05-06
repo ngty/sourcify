87d51939f1823b38cbc8be63b49d9728c226d3ae
Ng Tze Yang <ngty77@gmail.com>
2012-05-07 02:46:25 +0800

    Thorough cleanup of support for runnable fixtures.

--- spec/proc/spec_helper.rb
+++ spec/proc/spec_helper.rb
@@ -1,19 +1,1 @@
 require File.expand_path('../../spec_helper', __FILE__)
-
-module Sourcify
-  module Proc
-    module SpecHelper
-      def self.extended(example_group)
-        example_group.class_eval do
-
-          extend Sourcify::SpecHelper
-
-          def self.load_examples_file(name)
-            load ::File.expand_path("../#{name}_examples.rb", __FILE__)
-          end
-
-        end
-      end
-    end
-  end
-end
