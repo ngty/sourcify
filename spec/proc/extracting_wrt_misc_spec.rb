937561a3888611995c76f04366891375840a0532
Ng Tze Yang <ngty77@gmail.com>
2012-08-27 09:27:32 +0800

    Examples to cover misc (eg. encoding, comments, etc).

---
+++ spec/proc/extracting_wrt_misc_spec.rb
@@ -0,0 +1,55 @@
+# encoding: UTF-8
+require File.expand_path('../spec_helper', __FILE__)
+
+def puke(*args, &block)
+  block
+end
+
+describe Sourcify::Proc::Extractor do
+  describe '.process (wrt misc)' do
+    extend Sourcify::SpecHelper
+
+    def process(block)
+      Sourcify::Proc::Extractor.process(block).src
+    end
+
+    example(%%
+    ## wrt comments
+    ##
+    #" proc {
+    #"         # shouldnt remove me !!
+    #"         :thing
+    #"       }
+    %,(
+      b = proc {
+        # shouldnt remove me !!
+        :thing
+      }
+    ))
+
+    example(%%
+    ## wrt encoding, having utf8 string
+    ##
+    #" proc {
+    #"         "こんにちは"
+    #"       }
+    %,(
+      b = proc {
+        "こんにちは"
+      }
+    ))
+
+    example(%%
+    ## wrt encoding, having unicode regexp
+    ##
+    #" proc {
+    #"         /\p{Lu}/
+    #"       }
+    %,(
+      b = proc {
+        /\p{Lu}/
+      }
+    ))
+
+  end
+end
