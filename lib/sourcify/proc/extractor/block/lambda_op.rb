bbde2e131f6dc4cc52234a8bf644596c0b7714ea
Ng Tze Yang <ngty77@gmail.com>
2012-09-18 13:24:44 +0800

    Delegate block building to DoEnd & Brace, & customize the building of indented body.

--- lib/sourcify/proc/extractor/block/lambda_op.rb
+++ lib/sourcify/proc/extractor/block/lambda_op.rb
@@ -4,40 +4,54 @@ module Sourcify
       module Block
         class LambdaOp < Base
 
+          def initialize(*args)
+            @block = nil
+            super
+          end
+
+          def <<(token)
+            if @block
+              @block << token
+            else
+              case token[EVT]
+              when :tlambeg then @block = Brace.new(@type, token)
+              when :kw_do then @block = DoEnd.new(@type, token)
+              else super
+              end
+            end
+          end
+
           def lambda_op?
             true
           end
 
           def done?
             @done ||=
-              if %w(} end).include?(frags[-1]) && correct?(body)
-                !!(@body = indented_body)
+              if @block && @block.done?
+                !!(@body = finalize(indented_body))
               end
           end
 
           def body
-            @body || finalize(block)
+            @body
           end
 
         private
 
-          def block
-            case self.frags[1]
-            when '('
-              frags = self.frags.dup
-              i_rparen, i_do, i_lbrace = %w") do {".map{|s| frags.index(s) }
-              i_nl = frags[0..i_rparen].rindex("\n")
-              frags[i_nl] = "\\" + frags[i_nl] if i_nl
+          def indented_body
+            tokens, body = self.tokens, @block.body.sub(/^lambda\ +/,'')
+            start = @block.is_a?(Brace) ? '{' : 'do'
 
-              if i_do && (i_lbrace.nil? || i_do < i_lbrace)
-                 %( do |#{frags[2...i_rparen]*''}|#{frags[(i_do+1)..-1]*''})
-              else
-                 %( { |#{frags[2...i_rparen]*''}|#{frags[(i_lbrace+1)..-1]*''})
+            params =
+              if tokens[1][EVT] == :lparen
+                i_rparen = tokens.rindex{|t| t[EVT] == :rparen }
+                tokens[2 ... i_rparen].map{|t| t[EVT] == :nl ? ' ' : t[FRG] }.join
               end
-            when / +/
-              %(#{self.frags[1..-1]*''})
+
+            if params
+              body.sub(start, "#{start} |#{params}|")
             else
-              %( #{self.frags[1..-1]*''})
+              body
             end
           end
 
