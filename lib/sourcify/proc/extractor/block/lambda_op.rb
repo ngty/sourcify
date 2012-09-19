b019e3b772da5f18c33c87604acdfcfe0b7ee748
Ng Tze Yang <ngty77@gmail.com>
2012-09-19 13:15:26 +0800

    Cleaned up the messy derivation of indented body.

--- lib/sourcify/proc/extractor/block/lambda_op.rb
+++ lib/sourcify/proc/extractor/block/lambda_op.rb
@@ -41,26 +41,29 @@ module Sourcify
         private
 
           def indented_body
-            tokens, body = self.tokens, block.body.sub(/^lambda\ +/,'')
+            body = block.body.sub(/^lambda\ +/,'')
 
-            params =
-              if tokens[1].lparen?
-                i_rparen = tokens.rindex(&:rparen?)
-                t_evts = tokens[(i_rparen-2) ... i_rparen].map(&:evt)
-
-                if t_evts == [:nl, :sp]
-                  tokens[i_rparen-2].frag = "\\\n"
-                elsif t_evts[-1] == :nl
-                  tokens[i_rparen-1].frag = "\\\n"
-                end
-
-                tokens[2 ... i_rparen].map {|t| t.frag(block.indent) }.join
-              end
-
-            if params
+            if params = extract_params
               body.sub(block.first, "#{block.first} |#{params}|")
             else
               body
+            end
+          end
+
+          def extract_params
+            tokens = self.tokens
+
+            if tokens[1].lparen?
+              i_rparen = tokens.rindex(&:rparen?)
+              t_evts = tokens[(i_rparen-2) ... i_rparen].map(&:evt)
+
+              if t_evts == [:nl, :sp]
+                tokens[i_rparen-2].frag = "\\\n"
+              elsif t_evts[-1] == :nl
+                tokens[i_rparen-1].frag = "\\\n"
+              end
+
+              tokens[2 ... i_rparen].map {|t| t.frag(block.indent) }.join
             end
           end
 
