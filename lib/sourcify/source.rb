1904b2b7d3effe3efcdcebf1315bb6637d050fa9
Ng Tze Yang <ngty77@gmail.com>
2012-09-20 12:34:59 +0800

    Simplified Sourcify::Source to only customize strip & strip!

--- lib/sourcify/source.rb
+++ lib/sourcify/source.rb
@@ -1,49 +1,19 @@
+require 'forwardable'
+
 module Sourcify
   class Source < Struct.new(:raw, :stripped)
 
-    private :raw=, :stripped=
+    extend Forwardable
+    def_delegators :raw, *("".public_methods(false) - [:strip, :strip!])
 
-    def to_s
-      raw
-    end
+    private :raw=, :stripped=
 
     def strip
       Source.new(stripped, stripped)
     end
 
     def strip!
-      self.replace(strip)
-      nil
-    end
-
-    def to_str
-      raw
-    end
-
-    def <=>(other)
-      to_s <=> other.to_s
-    end
-
-    def ==(other)
-      to_s == other.to_s
-    end
-
-    "".public_methods(false).select{|m| m.to_s.end_with?('!') }.each do |m_bang|
-      next if [:strip!].include?(m_bang)
-      m = m_bang.to_s.sub('!','')
-
-      define_method(m_bang) do |*args|
-        self.replace(send(m))
-        nil
-      end
-    end
-
-    "".public_methods(false).reject{|m| m.to_s.end_with?('!') }.each do |m|
-      next if [:strip, :to_s, :to_str, :==, :<=>].include?(m)
-
-      define_method(m) do |*args|
-        Sourcify::Source.new(raw.send(m, *args), stripped.send(m, *args))
-      end
+      self.replace(strip); nil
     end
 
   end
--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -4,33 +4,9 @@ describe Sourcify::Source do
   extend Sourcify::SpecHelper
 
   before do
-    @raw = "lambda { :thing }"
-    @stripped = ":thing"
+    @raw, @stripped = "lambda { :thing }", ":thing"
     @source = Sourcify::Source.new(@raw.dup, @stripped.dup)
   end
-
-
-  describe '#to_s' do
-    it 'must return the raw form' do
-      @source.to_s.must_equal(@raw)
-    end
-
-    it 'must return instance of String' do
-      @source.to_s.must_be_instance_of(String)
-    end
-  end
-
-
-  describe '#to_str' do
-    it 'must return the raw form' do
-      @source.to_str.must_equal(@raw)
-    end
-
-    it 'must return instance of String' do
-      @source.to_str.must_be_instance_of(String)
-    end
-  end
-
 
   describe '#strip' do
     it 'must return the stripped form' do
@@ -42,7 +18,6 @@ describe Sourcify::Source do
     end
   end
 
-
   describe "#strip!" do
     it 'must return nil' do
       @source.strip!.must_equal(nil)
@@ -50,83 +25,35 @@ describe Sourcify::Source do
 
     it "must convert self to Sourcify::Source of stripped form" do
       @source.strip!
-      @source.must_equal(Sourcify::Source.new(@stripped, @stripped))
+      @source.must_be_instance_of(Sourcify::Source)
+      @source.must_equal(@stripped)
     end
   end
 
+
+  # Sparingly check for delegation to @raw
 
   describe '#<=>' do
-    it 'must return 1 if it is bigger than other' do
-      smaller = Sourcify::Source.new(@raw.upcase, @stripped.downcase)
-      (@source <=> smaller).must_equal(1)
+    it 'must return 1 if the raw form is bigger than other' do
+      (@source <=> @raw.upcase).must_equal(1)
     end
 
-    it 'must return -1 if it is smaller than other' do
-      smaller = Sourcify::Source.new(@raw.upcase, @stripped.downcase)
-      (smaller <=> @source).must_equal(-1)
+    it 'must return -1 if the raw form is smaller than other' do
+      (@raw.upcase <=> @source).must_equal(-1)
     end
 
-    it 'must return 0 if it is equal to other' do
-      same = Sourcify::Source.new(@raw, @stripped.upcase)
-      (@source <=> same).must_equal(0)
+    it 'must return 0 if the raw form is equal to other' do
+      (@source <=> @raw).must_equal(0)
     end
   end
-
 
   describe '#==' do
-    it 'must return true if other is a Sourcify::Source & has the same raw form' do
-      @source.must_equal(Sourcify::Source.new(@raw, "watever"))
+    it 'must return true if the raw form is the same as other' do
+      @source.must_equal(@raw)
     end
 
-    it 'must return false if other is a Sourcify::Source & has different raw form' do
-      @source.wont_equal(Sourcify::Source.new("watever", @stripped))
-    end
-
-    it 'must return true if the stringified other is the same as the raw form' do
-      (@source == :"#{@raw}").must_equal(true)
-    end
-
-    it 'must return false if the stringified other is different from the raw form' do
-      (@source == :watever).must_equal(false)
-    end
-  end
-
-
-  "".public_methods(false).select{|m| m.to_s.end_with?('!') }.each do |m_bang|
-    next if [:strip!].include?(m_bang)
-    next if "".method(m_bang).arity.nonzero? # keep it simple
-    m = m_bang.to_s.sub('!','')
-
-    describe "##{m_bang}" do
-      it 'must return nil' do
-        @source.send(m_bang).must_equal(nil)
-      end
-
-      it "must convert self to Sourcify::Source of #{m}" do
-        new_source = Sourcify::Source.new(@raw.send(m), @stripped.send(m))
-
-        @source.send(m_bang)
-        @source.must_equal(new_source)
-      end
-    end
-  end
-
-
-  "".public_methods(false).reject{|m| m.to_s.end_with?('!') }.each do |m|
-    next if [:to_s, :to_str, :strip, :==].include?(m)
-    next if [:bytes, :chars, :codepoints].include?(m) # keep it simple
-    next if m.to_s.start_with?('each_') # keep it simple
-    next if "".method(m).arity.nonzero? # keep it
-
-    describe "##{m}" do
-      it 'must return instance of Sourcify::Source' do
-        @source.send(m).must_be_instance_of(Sourcify::Source)
-      end
-
-      it "must return the #{m}-ed form" do
-        new_source = Sourcify::Source.new(@raw.send(m), @stripped.send(m))
-        @source.send(m).must_equal(new_source)
-      end
+    it 'must return false if the raw form is different from the other' do
+      @source.wont_equal(@stripped)
     end
   end
 
