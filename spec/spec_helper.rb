3a2f48e28ff37ac696c233ad191d436d604a2646
Ng Tze Yang <ngty77@gmail.com>
2012-09-23 02:05:15 +0800

    Added preliminary support for irb & pry specific examples.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,66 +1,110 @@
-require 'minitest/spec'
-require File.expand_path('../../lib/sourcify', __FILE__)
+SOURCIFY_RB = File.expand_path('../../lib/sourcify', __FILE__)
 
+require 'minitest/spec'
+require SOURCIFY_RB
 
 module Sourcify
   module SpecHelper
 
-    def example(paragraph, block)
-      description, expected = extract_args(paragraph)
-      debug_line = caller[0].split(':')[1]
-      description += " (at line #{debug_line})"
-
-      expected.each do |type, val|
-        send(:"example_for_#{type}", description, val, block)
+    def self.extended(base)
+      base.class_eval do
+        include InstanceMethods
+        extend ClassMethods
       end
     end
 
-  private
+    module InstanceMethods
 
-    def extract_args(paragraph)
-      lines = paragraph.split("\n")
-
-      description, evaluable, literal, error = %w{## #> #" #!}.map do |pfx|
-        pattern = /^ *#{pfx} /
-        lines.grep(pattern).map{|s| s.sub(pattern,'') }.join("\n")
+      def irb_exec(string)
+        eval_output <<-`SHELL`
+(
+cat <<-RUBY
+  #{string}
+RUBY
+) | irb -r #{SOURCIFY_RB} --simple-prompt
+        SHELL
       end
 
-      unless error.empty?
-        def error.to_constant
-          self.to_s.split('::').inject(Object) do |klass, const|
-            klass.const_get(const)
-          end
+      def pry_exec(string)
+        eval_output <<-`SHELL`
+(
+cat <<-RUBY
+  #{string}
+RUBY
+) | pry -r #{SOURCIFY_RB} --simple-prompt --no-color
+        SHELL
+      end
+
+    private
+
+      def eval_output(output)
+        lines = output.split("\n")
+        i = lines.rindex{|l| l.start_with?('=>') }
+        eval(lines[i .. -1].map{|l| l[3 .. -1] } * "\n")
+      end
+
+    end
+
+    module ClassMethods
+
+      def example(paragraph, block)
+        description, expected = extract_args(paragraph)
+        debug_line = caller[0].split(':')[1]
+        description += " (at line #{debug_line})"
+
+        expected.each do |type, val|
+          send(:"example_for_#{type}", description, val, block)
         end
       end
 
-      [
-        description,
-        {
-          :evaluable => evaluable,
-          :literal => literal,
-          :error => error
-        }.reject{|k,v| v.empty? }
-      ]
-    end
+    private
 
-  private
+      def extract_args(paragraph)
+        lines = paragraph.split("\n")
 
-    def example_for_evaluable(description, expected, block)
-      it description do
-        process(block).must_equal(eval(expected))
+        description, evaluable, literal, error = %w{## #> #" #!}.map do |pfx|
+          pattern = /^ *#{pfx} /
+          lines.grep(pattern).map{|s| s.sub(pattern,'') }.join("\n")
+        end
+
+        unless error.empty?
+          def error.to_constant
+            self.to_s.split('::').inject(Object) do |klass, const|
+              klass.const_get(const)
+            end
+          end
+        end
+
+        [
+          description,
+          {
+            :evaluable => evaluable,
+            :literal => literal,
+            :error => error
+          }.reject{|k,v| v.empty? }
+        ]
       end
-    end
 
-    def example_for_literal(description, expected, block)
-      it description do
-        process(block).must_equal(expected)
-      end
-    end
+    private
 
-    def example_for_error(description, expected, block)
-      it description do
-        lambda { process(block) }.must_raise(expected.to_constant)
+      def example_for_evaluable(description, expected, block)
+        it description do
+          process(block).must_equal(eval(expected))
+        end
       end
+
+      def example_for_literal(description, expected, block)
+        it description do
+          process(block).must_equal(expected)
+        end
+      end
+
+      def example_for_error(description, expected, block)
+        it description do
+          lambda { process(block) }.must_raise(expected.to_constant)
+        end
+      end
+
     end
 
   end
