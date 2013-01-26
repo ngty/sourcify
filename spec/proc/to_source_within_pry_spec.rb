eb0dcf6814abb614b832e8bef990da219963d308
Ng Tze Yang <ngty77@gmail.com>
2013-01-27 00:50:51 +0800

    HMM, OA isnt working the way it is expected (pry throws "NameError: uninitialized constant OA")

--- spec/proc/to_source_within_pry_spec.rb
+++ spec/proc/to_source_within_pry_spec.rb
@@ -40,17 +40,16 @@ describe "Proc#to_source within PRY" do
 
   # NOTE: The rest address issue#22, kudos goes to @scooter-dangle
   # for providing such detailed examples :)
-
-  should 'handle a single upward scroll' do
-    pry_eval(%Q(
-      a = lambda { |proc| proc.to_source }
-      a[(lambda { 0 })]
-      a[(lambda { 1 })]
-      OA
-    )).should.be equal_to('proc { 1 }')
-  end
-
-  # NOTE: Can't get double arrow up (OAOA) to work. 
+  #
+  #should 'handle a single upward scroll' do
+  #  pry_eval(%Q(
+  #    a = lambda { |proc| proc.to_source }
+  #    a[(lambda { 0 })]
+  #    a[(lambda { 1 })]
+  #    OA
+  #  )).should.be equal_to('proc { 1 }')
+  #end
+  #
   #should 'handle a double upward scroll' do
   #  pry_eval(%Q(
   #    a = lambda { |proc| proc.to_source }
